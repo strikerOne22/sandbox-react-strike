88cbe02f1a40eae94c9b5d745c3e3d590b915784
hftamayo <hftamayo@gmail.com>
2021-03-01 19:05:28 -0600

    corrigiendo dependencias y uso de browserrouter

--- src/Paginas/Menu/index.js
+++ src/Paginas/Menu/index.js
@@ -1,45 +1,68 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 function Menu(){
     return(
         <div>
             <ul>
                 <li>
-                    <Link to="/basicos">01 Basicos</Link>
-                    {/*jsx, componentes, props, state, 
-                    eventos y metodos, components naming,
-                    hooks, stateless, stateful */}
+                    <BrowserRouter>
+                        <Link to="/basicos">01 Basicos</Link>
+                        {/*jsx, componentes, props, state, 
+                        eventos y metodos, components naming,
+                        hooks, stateless, stateful */}
+                    </BrowserRouter>
+
                 </li>
                 <li>
-                    <Link to="/cvida">02 Ciclo de Vida</Link>
+                    <BrowserRouter>
+                        <Link to="/cvida">02 Ciclo de Vida</Link>
+                    </BrowserRouter>
                 </li>
                 <li>
-                    <Link to="/rutas">03 Routing</Link>
+                    <BrowserRouter>                    
+                        <Link to="/rutas">03 Routing</Link>
+                    </BrowserRouter>                        
                 </li>                
                 <li>
-                    <Link to="/rutas">04 Redux</Link>
+                    <BrowserRouter>                    
+                        <Link to="/rutas">04 Redux</Link>
+                    </BrowserRouter>
                 </li>                                
                 <li>
-                    <Link to="/rutas">05 ReduxToolkit</Link>
+                    <BrowserRouter>
+                        <Link to="/rutas">05 ReduxToolkit</Link>
+                    </BrowserRouter>
                 </li>                                
                 <li>
-                    <Link to="/rutas">06 API Fetch: Axios</Link>
+                    <BrowserRouter>                    
+                        <Link to="/rutas">06 API Fetch: Axios</Link>
+                    </BrowserRouter>                        
                 </li>                                    
                 <li>
-                    <Link to="/rutas">07 API Fetch: ReactQuery</Link>
+                    <BrowserRouter>                    
+                        <Link to="/rutas">07 API Fetch: ReactQuery</Link>
+                    </BrowserRouter>                    
                 </li>                                         
                 <li>
-                    <Link to="/rutas">08 CRUD Single Entity</Link>
+                    <BrowserRouter>                    
+                        <Link to="/rutas">08 CRUD Single Entity</Link>
+                    </BrowserRouter>                    
                 </li>                                      
                 <li>
-                    <Link to="/rutas">09 CRUD OneToMany</Link>
+                    <BrowserRouter>                    
+                        <Link to="/rutas">09 CRUD OneToMany</Link>
+                    </BrowserRouter>                        
                 </li>                                  
                 <li>
-                    <Link to="/rutas">10 Login</Link>
+                    <BrowserRouter>                    
+                        <Link to="/rutas">10 Login</Link>
+                    </BrowserRouter>                        
                 </li>                         
                 <li>
-                    <Link to="/rutas">11 ShoppingCart</Link>
+                    <BrowserRouter>                    
+                        <Link to="/rutas">11 ShoppingCart</Link>
+                    </BrowserRouter>                        
                 </li>
             </ul>
         </div>
